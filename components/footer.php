ec933fee8666988538974fa4a038b24905a771c5
sakumahideyuki-3939 <sakumahideyuki@gmail.com>
2026-02-08 09:27:53 +0900

    chore: update footer marker

--- components/footer.php
+++ components/footer.php
@@ -4,6 +4,7 @@
 <footer>
   <div class="container">© <?php echo date('Y'); ?> algo-site</div>
 </footer>
-<script src="/assets/js/main.js"></script>
+<script src="/assets/js/main.js"></script><br />
+<!-- updated: 2026-02-08 -->
 </body>
 </html>
