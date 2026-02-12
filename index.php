40ea53d52094a963ae22c9de73c2025d7e5a2725
sakumahideyuki-3939 <sakumahideyuki@gmail.com>
2026-02-12 15:36:59 +0900

    Fix include path for page_nav in index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $page_title = "トップ";
 require __DIR__ . "/config.php";
 include __DIR__ . "/components/header.php";
-include __DIR__ . "/components/page_nav.php";
+include __DIR__ . "/pages/page_nav.php";
 ?>
 <main class="container">
   <h1>トップページ</h1>
