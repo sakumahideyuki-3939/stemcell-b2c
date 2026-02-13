7261e525ed1efbdef939c1cd99123dee3b7eaca5
sakumahideyuki-3939 <sakumahideyuki@gmail.com>
2026-02-13 13:28:14 +0900

    Finalize work page split structure

---
+++ work04.php
@@ -0,0 +1,13 @@
+<?php
+$page_title = "実績";
+require __DIR__ . "/config.php";
+include __DIR__ . "/components/header.php";
+include __DIR__ . "/components/page_nav.php";
+?>
+<?php include __DIR__ . "/components/layout_open.php"; ?>
+<main class="container">
+  <h1>実績</h1>
+  <?php include __DIR__ . "/pages/works/work-1.php"; ?>
+</main>
+<?php include __DIR__ . "/components/footer.php"; ?>
+<?php include __DIR__ . "/components/layout_close.php"; ?>
