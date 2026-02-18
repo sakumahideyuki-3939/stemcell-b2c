ec30f8a4fd270aaef8a70d282eae3a1f9ee59f5d
sakumahideyuki-3939 <sakumahideyuki@gmail.com>
2026-02-18 21:08:39 +0900

    Update header nav, footer links and copy to match final content structure

--- components/footer.php
+++ components/footer.php
@@ -13,23 +13,25 @@
         <div class="c-footer__heading">Navigate</div>
         <ul class="c-footer__list">
           <li><a href="./index.php">HOME</a></li>
-          <li><a href="./concept.php">CONCEPT</a></li>
-          <li><a href="./evidence.php">EVIDENCE</a></li>
-          <li><a href="./company.php">COMPANY</a></li>
-          <li><a href="./contact.php">CONTACT</a></li>
+          <li><a href="./work01.php">はじめての方へ</a></li>
+          <li><a href="./about.php">仕組みを知る</a></li>
+          <li><a href="./work03.php">安全性</a></li>
+          <li><a href="./work04.php">費用</a></li>
+          <li><a href="./work07.php">無料相談</a></li>
         </ul>
       </div>
       <div>
-        <div class="c-footer__heading">Products</div>
+        <div class="c-footer__heading">Contents</div>
         <ul class="c-footer__list">
-          <li><a href="./work.php">WORKS</a></li>
-          <li><a href="./blog/">BLOG</a></li>
+          <li><a href="./work.php">コンテンツ一覧</a></li>
+          <li><a href="./evidence.php">根拠・研究</a></li>
+          <li><a href="./concept.php">相談者の声</a></li>
         </ul>
       </div>
       <div>
         <div class="c-footer__heading">cells.algo-cosme</div>
         <p style="font-size:var(--fs-caption);opacity:0.72;line-height:var(--lh-body);">
-          脂肪組織や骨髄由来の幹細胞を高度に濾過した「濾液」を活用する、革新的な無細胞療法です。
+          幹細胞治療の情報整理と、あなたに合う医療機関への相談・紹介をサポートします。押し売りのない相談窓口です。
         </p>
       </div>
     </div>
--- components/header.php
+++ components/header.php
@@ -9,13 +9,11 @@
 
     <nav class="algo-header__nav" aria-label="Global navigation">
       <ul class="algo-header__navlist">
-        <li><a href="./index.php">家</a></li>
-        <li><a href="./about.php">について</a></li>
-        <li><a href="./concept.php">コンセプト</a></li>
-        <li><a href="./evidence.php">証拠</a></li>
-        <li><a href="./company.php">会社</a></li>
-        <li><a class="is-cta" href="./contact.php">接触</a></li>
-        <li><a href="./blog/">ブログ</a></li>
+        <li><a href="./about.php">仕組みを知る</a></li>
+        <li><a href="./work03.php">安全性</a></li>
+        <li><a href="./work04.php">費用</a></li>
+        <li><a href="./evidence.php">根拠</a></li>
+        <li><a class="is-cta" href="./work07.php">無料相談</a></li>
       </ul>
     </nav>
 
